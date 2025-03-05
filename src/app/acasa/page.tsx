88cc5602a1722500e633a23af0e584e68c8e6ef1
Gabriel Voicu <gabriel.voicu@dipocket.org>
2025-03-05 11:28:08 +0200

    scos in lucru

--- src/app/acasa/page.tsx
+++ src/app/acasa/page.tsx
@@ -14,7 +14,7 @@ export default function Acasa() {
             <div className="container flex flex-col flex-grow sm:flex-row bg-white">
                 <main className="flex-1 p-6 bg-white border-b sm:border-b-0 sm:border-r">
                     <h1 className="text-2xl font-bold mb-4">Acasa</h1>
-                    <p>In lucru</p>
+                    <p>-</p>
                 </main>
                 <Sidebar />
             </div>
