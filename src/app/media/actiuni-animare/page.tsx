9571a1e233c8c701c3e69655796bc63c60f18b9b
Gabriel Voicu <gabrielvoicujpi@gmail.com>
2025-10-15 09:17:07 +0300

    plan bune practici

--- src/app/media/actiuni-animare/page.tsx
+++ src/app/media/actiuni-animare/page.tsx
@@ -15,7 +15,8 @@ export default function Acasa() {
             <div className="container flex flex-col flex-grow sm:flex-row bg-white">
                 <main className="flex-1 p-6 bg-white border-b sm:border-b-0 sm:border-r">
                     <h1 className="text-2xl font-bold mb-4">Actiuni animare</h1>
-                    <h2 className="text-xl font-bold mb-4">Anunturi - trimestrul al IV-lea - anul 1</h2>
+                    <Link className="text-blue-600 underline hover:text-blue-800" href="/media/plan-bune-practici.pdf">Plan de bune practici</Link>
+                    <h2 className="text-xl font-bold mb-4 mt-4">Anunturi - trimestrul al IV-lea - anul 1</h2>
                     <ul className="list-none list-inside">
                         <li>1. <Link className="text-blue-600 underline hover:text-blue-800" href="/media/actiuni-animare/anunturi-an-1-sem-4/anunt-aliman.docx">COMUNA ALIMAN</Link></li>
                         <li>2. <Link className="text-blue-600 underline hover:text-blue-800" href="/media/actiuni-animare/anunturi-an-1-sem-4/anunt-ciocirlia.docx">COMUNA CIOCIRLIA</Link></li>
