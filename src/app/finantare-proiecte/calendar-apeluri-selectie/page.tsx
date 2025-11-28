7592245983c41b71d31e49f125a4ebfa0a3fb9af
Gabriel Voicu <gabrielvoicujpi@gmail.com>
2025-11-28 13:51:31 +0200

    calendare

--- src/app/finantare-proiecte/calendar-apeluri-selectie/page.tsx
+++ src/app/finantare-proiecte/calendar-apeluri-selectie/page.tsx
@@ -16,7 +16,7 @@ export default function CalendarApeluriSelectie() {
                 <main className="flex-1 p-6 bg-white border-b sm:border-b-0 sm:border-r">
                     <h1 className="text-2xl font-bold mb-4">Calendar apeluri selectie</h1>
                     <ul className="list-none list-inside">
-                        <li><Link className="text-blue-600 underline hover:text-blue-800" href="/media/calendar estimativ DR 36 AN 1 trim 4 r.jpg" target="_blank">calendar-AM-estimativ-an-1-trim-IV-2025</Link></li>
+                        <li><Link className="text-blue-600 underline hover:text-blue-800" href="/media/calendar-estimativ-DR-36-AN-2.pdf" target="_blank">CALENDAR ESTIMATIV DR 36 AN 2</Link></li>
                     </ul>
                 </main>
                 <Sidebar />
