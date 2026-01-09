ed2263d26f1f6efaa9490906fd936ba40887aa83
Gabriel Voicu <gabrielvoicujpi@gmail.com>
2026-01-09 10:17:45 +0200

    adaugare comunicat

--- src/app/media/comunicate/page.tsx
+++ src/app/media/comunicate/page.tsx
@@ -15,8 +15,9 @@ export default function Comunicate() {
             <div className="container flex flex-col flex-grow sm:flex-row bg-white">
                 <main className="flex-1 p-6 bg-white border-b sm:border-b-0 sm:border-r">
                     <h1 className="text-2xl font-bold mb-4">Comunicate</h1>
+                    <Link className="text-blue-600 underline hover:text-blue-800" href="/media/comunicate/NOTA-PRELUNGIRE-PERIOADA-DE-EVALUARE.pdf">NOTA PRELUNGIRE PERIOADA DE EVALUARE</Link><br/><br/>
                     <Link className="text-blue-600 underline hover:text-blue-800" href="/media/comunicate/1-anunt-apel-interventia-5.docx">APEL SELECTIE INTERVENTIA NR 5 START-UP NONAGRICOL</Link><br/><br/>
-                     <Link className="text-blue-600 underline hover:text-blue-800" href="/media/comunicate/2-anunt-prelungire-interventia-5.docx">PRELUNGIRE APEL SELECTIE INTERVENTIA NR 5 START-UP NONAGRICOL</Link>
+                    <Link className="text-blue-600 underline hover:text-blue-800" href="/media/comunicate/2-anunt-prelungire-interventia-5.docx">PRELUNGIRE APEL SELECTIE INTERVENTIA NR 5 START-UP NONAGRICOL</Link>
                 </main>
                 <Sidebar />
             </div>
