e4809e17690bbd04b3e8e216042ef9479c2d0af4
Gabriel Voicu <gabrielvoicujpi@gmail.com>
2026-02-16 15:54:59 +0200

    update

--- src/app/arhiva/page.tsx
+++ src/app/arhiva/page.tsx
@@ -15,6 +15,10 @@ export default function Arhiva() {
             <div className="container flex flex-col flex-grow sm:flex-row bg-white">
                 <main className="flex-1 p-6 bg-white border-b sm:border-b-0 sm:border-r">
                     <h1 className="text-2xl font-bold mb-4">Arhiva</h1>
+                    <h1 className="text-2xl font-bold mb-4">Apeluri selectie</h1>
+                    <Link className="text-blue-600 underline hover:text-blue-800" href="/media/Raport-selectie-intermediar-I5-DC.pdf" target="_blank">Raport-selectie-intermediar-I5-DC</Link><br />
+                    <Link className="text-blue-600 underline hover:text-blue-800" href="/media/documente-apel strat - up 01.zip" target="_blank">documente-apel strat - up 01</Link><br />
+
                     <h1 className="text-2xl font-bold mb-4">Calendar apeluri selectie</h1>
                     <ul className="list-none list-inside">
                         <li><Link className="text-blue-600 underline hover:text-blue-800" href="/media/calendar estimativ DR 36 AN 1 trim 4 r.jpg" target="_blank">calendar-AM-estimativ-an-1-trim-IV-2025</Link></li>
